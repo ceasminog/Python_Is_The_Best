af3254b09a157851ea3d48953da8d7a52bd11ce6
ceasminog <32290501+ceasminog@users.noreply.github.com>
2018-04-21 11:13:35 +0300

    Delete Shrimp.py

--- Shrimp.py
+++
@@ -1,8 +0,0 @@
-def CreateShrimpOrNot(shrimpneighbours):
-    return shrimpneighbours == 3
-
-def ShouldSrimpDie(shrimpneig):
-    if (shrimpneig > 1 and shrimpneig < 4):
-        return False
-    else:
-        return True
